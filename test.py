480b9663e159c27e10df411ca9ba53f6b68fa58b
tiena2cva <tiena2cva@gmail.com>
2017-12-11 23:20:42 +0700

    update option tessedit_char_whitelist for tesseract

--- test.py
+++ test.py
@@ -1,18 +1,7 @@
+import tesserocr
 from PIL import Image
-import pytesseract
-import argparse
-import cv2
-import os
 
-image = cv2.imread("example_01.png")
-gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+image = Image.open('text0_bolder.png')
 
-filename = "{}.png".format(os.getpid())
-cv2.imwrite(filename, gray)
-print(filename)
-
-image_gray = Image.open(filename)
-
-text = pytesseract.image_to_string(image_gray)
-
+text = tesserocr.image_to_text(image)  # print ocr text from image
 print(text)
